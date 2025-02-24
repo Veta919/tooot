5e8910786f21e5d684222c6d8354491f867a1e51
Veta919 <kiri0worin@gmail.com>
2025-02-24 11:09:31 +0300

    Add files via upload
    код с заданием

---
+++ Untitled0.cpp
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x, y;
+    printf("vvedite znacenia x: ");
+    scanf("%d", &x);
+    y = (5 * x + 20) * 2;
+    printf("rezultat: y = %d\n", y);
+    return 0;
+}
